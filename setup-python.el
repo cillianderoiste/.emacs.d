3492d33740b1435c87866e48c29bdcb9b76d6062
Florian Friesdorf <flo@chaoflow.net>
2013-03-31 20:58:06 +0200

    basic python-flake8 and python-pylint
    check also ~/bin/{mrsd_run|flake8|pylint} in my skel

--- setup-python.el
+++ setup-python.el
@@ -2,4 +2,12 @@
 
 (add-hook 'python-mode-hook 'whitespace-mode)
 
+(autoload 'python-pylint "python-pylint")
+(autoload 'pylint "python-pylint")
+(autoload 'python-flake8 "python-flake8")
+(autoload 'flake8 "python-flake8")
+
+(require 'python-pylint)
+(require 'python-flake8)
+
 (provide 'setup-python)
